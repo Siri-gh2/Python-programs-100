2812c12f3a77834c23a0ad2d32c2860f2c458f5d
Korupolu Siri <smilewithu1436@gmail.com>
2026-07-01 23:12:37 +0530

    Implement string reversal without slicing
    Added a method to reverse a string without slicing.

--- 04_reverse_a_string.py
+++ 04_reverse_a_string.py
@@ -6,3 +6,16 @@ s = input()
 print("reverse of the string s is:",s[::-1])
 
 #time and space complexity is O(n)
+
+
+#without slicing
+
+s = input()
+rev = ""
+
+for ch in s:
+  rev = ch + rev
+
+print(rev)
+
+# time complexity = o(n^2)
